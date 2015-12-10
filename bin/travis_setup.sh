f07de11d046998ffd3f58995dcb40f979be9c1b1
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-10 10:40:17 -0500

    Use https for git clone

--- bin/travis_setup.sh
+++ bin/travis_setup.sh
@@ -3,7 +3,7 @@ ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 cat ~/.ssh/id_rsa.pub | sudo tee -a ~root/.ssh/authorized_keys
 
 # Project
-git clone git@github.com:dkniffin/vanilla-rails-app.git ~/vanilla-rails-app
+git clone https://github.com/dkniffin/vanilla-rails-app.git ~/vanilla-rails-app
 cd ~/vanilla-rails-app/
 
 # Set up tape
@@ -13,5 +13,5 @@ echo 'y' | tape installer install
 echo '[omnibox]' > hosts
 echo '0.0.0.0 be_app_env=production be_app_branch=master' >> hosts
 sed -i 's/app_name:.*/app_name: vanilla/g' tape_vars.yml
-sed -i 's/be_app_repo:.*/be_app_repo: git@github.com:dkniffin\/vanilla-rails-app.git/g' tape_vars.yml
+sed -i 's/be_app_repo:.*/be_app_repo: https:\/\/github.com\/dkniffin\/vanilla-rails-app.git/g' tape_vars.yml
 cp ~/.ssh/id_rsa.pub dev_keys/key.pub
