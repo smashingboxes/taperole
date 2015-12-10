b654c03f68bec9394052fde4cf4f164edd309ba5
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-10 09:36:22 -0500

    Initial setup of travis configs

---
+++ bin/travis_setup.sh
@@ -0,0 +1,28 @@
+# rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Ruby
+rbenv install 2.2.3
+# gem install bundler
+
+# setup ssh key
+ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+cat ~/.ssh/id_rsa.pub | sudo tee -a ~root/.ssh/authorized_keys
+
+# Project
+git clone git@github.com:dkniffin/vanilla-rails-app.git ~/vanilla-rails-app
+cd ~/vanilla-rails-app/
+
+# Set up tape
+gem install --local $TRAVIS_BUILD_DIR/taperole.gem
+echo 'y' | tape installer install
+# TODO: add a silent flag for ^
+echo '[omnibox]' > hosts
+echo '0.0.0.0 be_app_env=production be_app_branch=master' >> hosts
+sed -i 's/app_name:.*/app_name: vanilla/g' tape_vars.yml
+sed -i 's/be_app_repo:.*/be_app_repo: git@github.com:dkniffin\/vanilla-rails-app.git/g' tape_vars.yml
+cp ~/.ssh/id_rsa.pub dev_keys/key.pub
