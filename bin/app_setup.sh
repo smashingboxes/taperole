a672a03963935326b23ec3d269589808791242a0
Derek Kniffin <derek.kniffin@gmail.com>
2015-12-10 13:44:37 -0500

    Add step to add host keys to known_hosts

--- bin/app_setup.sh
+++ bin/app_setup.sh
@@ -1,6 +1,9 @@
 # setup ssh key
 ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 cat ~/.ssh/id_rsa.pub | sudo tee -a ~root/.ssh/authorized_keys
+ssh-keyscan -H 0.0.0.0 >> ~/.ssh/known_hosts
+ssh-keyscan -H 127.0.0.1 >> ~/.ssh/known_hosts
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
 
 # Project
 git clone https://github.com/dkniffin/vanilla-rails-app.git ~/vanilla-rails-app
