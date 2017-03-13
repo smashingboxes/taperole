ab49223f0ecab11fd718b64a50725cfe4bd8e63a
Brandon Mathis <bemathis@gmail.com>
2017-03-13 15:51:16 -0400

    Better start_rails test script

--- test/rails/start_rails.sh
+++ test/rails/start_rails.sh
@@ -3,8 +3,7 @@ rm -rf /home/deployer/vanilla/pids/puma.pid
 rm -rf /home/deployer/vanilla/pids/puma.state
 rm -rf /home/deployer/vanilla/tmp/*.sock
 service postgresql start
-service monit start
 monit start puma
 monit start nginx
-sleep 5
+sleep 2
 curl https://localhost --insecure
